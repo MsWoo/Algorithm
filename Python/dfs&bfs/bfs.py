3f3f4b2efd202134d92a6805648bee2252a2e2ae
MsWoo <minsoo010@naver.com>
2021-08-04 17:57:01 +0900

    python study
    dfs & bfs

---
+++ Python/dfs&bfs/bfs.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  4 16:58:59 2021
+
+@author: USER
+"""
+
+from collections import deque
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    
+    while queue:
+        v = queue.popleft()
+        print(v, end=" ")
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+    
+graph = [
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+
+visited = [False] * 9
+
+bfs(graph, 1, visited)
