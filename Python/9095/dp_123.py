afb79af579177c3b4f17fe44ad065c092d4e5b68
MsWoo <minsoo010@naver.com>
2021-10-17 23:13:04 +0900

    Create dp_123.py

---
+++ Python/9095/dp_123.py
@@ -0,0 +1,15 @@
+n = int(input())
+m = [int(input()) for _ in range(n)]
+
+dp = [-1 for _ in range(11)]
+
+dp[0] = 0
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in range(4,11):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+
+for i in m:
+    print(dp[i])
