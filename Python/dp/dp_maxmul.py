8839e73fd70cedbfd4a9147a70cba6be426bdd5e
MsWoo <minsoo010@naver.com>
2021-10-22 17:27:49 +0900

    Update dp_maxmul.py

--- Python/dp/dp_maxmul.py
+++ Python/dp/dp_maxmul.py
@@ -1,26 +1,14 @@
+#최대곱구간
 A = list(map(int, input().split()))
 
-dp = [1 for _ in range(len(A)+1)]
+dp_plus = [1 for _ in range(len(A))]
+dp_minus = [1 for _ in range(len(A))]
 
-dp[0] = 0
+dp_plus[0] = A[0]
+dp_minus[0] = A[0]
 
-startidx = 0
-minuscnt = 0
+for i in range(1,len(A)):
+    dp_plus[i] = max(A[i], max(dp_plus[i-1]*A[i], dp_minus[i-1]*A[i]))
+    dp_minus[i] = min(A[i], min(dp_plus[i-1]*A[i], dp_minus[i-1]*A[i]))
 
-for ele in A:
-    if ele < 0:
-        minuscnt+=1
-
-
-for i in range(1, len(A)+1):
-    for j in range(startidx, i):
-        if A[j] == 0:
-            startidx = j+1
-            dp[i] = 0
-        else :
-            if A[j]< 0:
-                dp[i] = max(dp[i], dp[i]*A[j]*-1)
-            else:
-                dp[i] = max(dp[i], dp[i]*A[j])
-
-print(dp)
+print(max(dp_plus))
