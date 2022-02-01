42852b97097bc44ca095cfb12fbd1f41e98919f5
MsWoo <minsoo010@naver.com>
2022-02-01 21:22:41 +0900

    이진 변환 반복하기
    Lv.2 월간코드챌린지 시즌1
    String.replaceAll
    Integer.toBinaryString

---
+++ Java/programmers/lv2/이진변환반복하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = {0, 0};
+
+        while(!s.equals("1")){
+            for(int i=0;i<s.length();i++){
+                if(s.charAt(i) == '0'){
+                    answer[1]++;
+                }
+            }
+            s = s.replaceAll("0", "");
+            
+            s = Integer.toBinaryString(s.length());
+            
+            answer[0];
+        }
+
+        return answer;
+    }
+}
